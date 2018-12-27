3e17fb4d439bb2cc64fad2fca28c0cf99fe8bdb0
Kemberlite4218 <RCstuvjsmh@gmail.com>
2018-12-27 14:51:27 +0800

    B10331008
    Signed-off-by: Kemberlite4218 <RCstuvjsmh@gmail.com>

---
+++ TCard_demo/src/test/java/com/TCard/demo/TCardDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.TCard.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TCardDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
