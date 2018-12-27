a3f3da949d241092fa4c7ca56525353daead3a5e
ccachx <741067444@qq.com>
2018-12-27 13:46:13 +0800

    ccx tpage1.0

---
+++ src/main/java/com/TCard/demo/TCardDemoApplication.java
@@ -0,0 +1,14 @@
+package com.TCard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TCardDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TCardDemoApplication.class, args);
+	}
+
+}
+
