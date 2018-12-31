0e2ab745ac2d3a7399a5b0db688c2ff278309d59
ccachx <741067444@qq.com>
2018-12-31 12:19:19 +0800

    F10715057

---
+++ src/main/java/com/TCard/demo/TCardDemoApplication.java
@@ -0,0 +1,18 @@
+package com.TCard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+//,scanBasePackages={"com.TCard.demo"}
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@EnableJpaRepositories ("com.TCard.demo.*") // this fix the problem
+public class TCardDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TCardDemoApplication.class, args);
+	}
+
+}
