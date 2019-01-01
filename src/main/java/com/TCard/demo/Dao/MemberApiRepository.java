05f322f263753607588a2a767b6e99f1f6ff3a95
ccachx <741067444@qq.com>
2019-01-01 17:09:43 +0800

    B10331008
    Signed-off-by: Kemberlite4218 <RCstuvjsmh@gmail.com>

--- src/main/java/com/TCard/demo/Dao/MemberApiRepository.java
+++ src/main/java/com/TCard/demo/Dao/MemberApiRepository.java
@@ -1,15 +1,18 @@
 package com.TCard.demo.Dao;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
+import org.springframework.data.jpa.repository.Query;
 import com.TCard.demo.Model.MemberAccount;
 
-@Component
-@Qualifier("MemberApiRepository")
-public interface MemberApiRepository  extends JpaRepository<MemberAccount, Long>{
 
+//@Component
+//@Qualifier("MemberApiRepository")
+public interface MemberApiRepository  extends JpaRepository<MemberAccount, Long>{
+	List<MemberAccount> findAll();
+	List<MemberAccount> findByEmail(String email);
+	@Query(value="select id,email,password,address,cellphone from member.memberaccountjpa where EMAIL = ?1 and PASSWORD = ?2 " ,nativeQuery = true)
+	List<MemberAccount> findCheckMemberAccount(String email,String password);
 
 	//MemberAccount findByIdStartsWith(String id);
 	
